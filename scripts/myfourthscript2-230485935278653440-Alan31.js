fd522db4d60d669297f41221e257b541636581c1
? <?@7307e514-9437-4341-b29e-66cd513812fe>
2021-07-24 06:42:10 +0000

    Update scripts

---
+++ scripts/myfourthscript2-230485935278653440-Alan31.js
@@ -0,0 +1,10 @@
+var onPre = function onPre() {
+	if (player.onGround) {
+		player.jump();
+		player.motionY = 0.4;
+		player.motionX *= 2;
+		player.motionZ *= 2;
+	} else {
+		player.motionY = -1;
+	}
+}
